0c71d9473c467253729d8abcb7525b6af5f5fc18
allencho <allen.my.cho@gmail.com>
2019-02-20 16:13:26 +1100

    Set up DB and create Angular app
    Migration done initially and just created Angular app

---
+++ TheFlirters.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace TheFlirters.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name{ get; set; }
+    }
+}
---
+++ TheFlirters.API/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace TheFlirters.API
+{
+    public class Program
+    {
+        // Entry point
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
