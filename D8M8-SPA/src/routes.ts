93afa0e2c2af28dcfcb5694d64a7d114afdd4d5b
allencho <allen.my.cho@gmail.com>
2019-03-09 17:53:59 +1100

    user interface done

--- D8M8-SPA/src/routes.ts
+++ D8M8-SPA/src/routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
-import { MemberListComponent } from './app/member-list/member-list.component';
+import { MemberListComponent } from './app/members/member-list/member-list.component';
 import { HomeComponent } from './app/home/home.component';
 import { MessagesComponent } from './app/messages/messages.component';
 import { ListsComponent } from './app/lists/lists.component';
 import { AuthGuard } from './app/_guards/auth.guard';
+import { MemberDetailComponent } from './app/members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './app/_resolvers/member-detail.resolver';
+import { MemberListResolver } from './app/_resolvers/member-list.resolver';
 
 // Ordering is important
 export const appRoutes: Routes = [
@@ -16,7 +19,8 @@ export const appRoutes: Routes = [
         runGuardsAndResolvers: 'always',
         canActivate: [AuthGuard],
         children: [
-            { path: 'members', component: MemberListComponent },
+            { path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver} },
+            { path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver} },
             { path: 'messages', component: MessagesComponent },
             { path: 'lists', component: ListsComponent },
         ]
