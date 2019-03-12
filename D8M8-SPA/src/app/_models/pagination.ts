ecdeadb60efe3e7df3b5682297cc510c7f12c5e2
allencho <allen.my.cho@gmail.com>
2019-03-12 17:01:28 +1100

    end of the functionalities

--- D8M8-SPA/src/app/_models/pagination.ts
+++ D8M8-SPA/src/app/_models/pagination.ts
@@ -9,15 +9,3 @@ export class PaginatedResult<T> {
     result: T;
     pagination: Pagination;
 }
-
-export interface Pagination {
-    currentPage: number;
-    itemsPerPage: number;
-    totalItems: number;
-    totalPages: number;
-}
-
-export class PaginatedResult<T> {
-    result: T;
-    pagination: Pagination;
-}
